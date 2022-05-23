4d229ff107d67cefe8c0f3ae9cda8e58ebbcf3d5
marianatassan <marianatassan19@gmail.com>
2022-05-23 15:25:40 -0300

    feito até matrizes dos 11 primeiros pontos de pico

--- codigo.py
+++ codigo.py
@@ -11,7 +11,7 @@ ds_hem = os.listdir(filepath_hematita)
 tam_hem = len(ds_hem)
 
 data_hem = np.zeros((tam_hem, len(theta)))
-#np.set_printoptions(threshold=sys.maxsize)
+hem_feature = []
 
 for i in range(0, tam_hem):
     df = pd.read_csv('Base_Dados/Hematita/' + ds_hem[i], sep=';')
@@ -20,16 +20,18 @@ for i in range(0, tam_hem):
         for k in range(len(theta)): 
             if df['2-THETA'][j] == theta[k]:
                 data_hem[i][k] = df['2-THETA'][j]
-                
-       
+    for m in range(10):
+      hem_feature.append(df['INTENSITY'][m])
+    hem_feature.append(len(df['INTENSITY']))
+
 # Magnetita
 
 filepath_magnetita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_17_05_2022/Base_Dados/Magnetita'
 ds_mag = os.listdir(filepath_magnetita)
 tam_mag = len(ds_mag)
 
-data_mag = np.zeros((15,3), dtype=np.float64)
-#np.set_printoptions(threshold=sys.maxsize)
+data_mag = np.zeros((tam_mag, len(theta)))
+mag_feature = []
 
 for i in range(0, tam_mag):
     df = pd.read_csv('Base_Dados/Magnetita/' + ds_mag[i], sep=';')
@@ -38,6 +40,9 @@ for i in range(0, tam_mag):
         for k in range(len(theta)): 
             if df['2-THETA'][j] == theta[k]:
                 data_mag[i][k] = df['2-THETA'][j]
+    for m in range(10):
+      mag_feature.append(df['INTENSITY'][m])
+    mag_feature.append(len(df['INTENSITY']))
 
 # Goetita
 
@@ -45,8 +50,8 @@ filepath_goetita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_
 ds_goe = os.listdir(filepath_goetita)
 tam_goe = len(ds_goe)
 
-data_goe = np.zeros((15,3), dtype=np.float64)
-#np.set_printoptions(threshold=sys.maxsize)
+data_goe = np.zeros((tam_goe, len(theta)))
+goe_feature = []
 
 for i in range(0, tam_goe):
     df = pd.read_csv('Base_Dados/Goetita/' + ds_goe[i], sep=';')
@@ -54,7 +59,10 @@ for i in range(0, tam_goe):
     for j in range(len(df['2-THETA'])):
         for k in range(len(theta)): 
             if df['2-THETA'][j] == theta[k]:
-                data_goe[i][k] = df['2-THETA'][j]      
+                data_goe[i][k] = df['2-THETA'][j]    
+    for m in range(10):
+      goe_feature.append(df['INTENSITY'][m])
+    goe_feature.append(len(df['INTENSITY']))  
 
 # Calcita
 
@@ -62,8 +70,8 @@ filepath_calcita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_
 ds_cal = os.listdir(filepath_calcita)
 tam_cal = len(ds_cal)
 
-data_cal = np.zeros((15,3), dtype=np.float64)
-#np.set_printoptions(threshold=sys.maxsize)
+data_cal = np.zeros((tam_cal, len(theta)))
+cal_feature = []
 
 for i in range(0, tam_cal):
     df = pd.read_csv('Base_Dados/Calcita/' + ds_cal[i], sep=';')
@@ -72,3 +80,6 @@ for i in range(0, tam_cal):
         for k in range(len(theta)): 
             if df['2-THETA'][j] == theta[k]:
                 data_cal[i][k] = df['2-THETA'][j] 
+    for m in range(10):
+      cal_feature.append(df['INTENSITY'][m])
+    cal_feature.append(len(df['INTENSITY'])) 
