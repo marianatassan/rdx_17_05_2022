964ba8f14ba29238d96bdfa1131f3e29ad484de3
marianatassan <marianatassan19@gmail.com>
2022-05-29 19:11:02 -0300

    nome_arquivo

--- codigo.py
+++ codigo.py
@@ -6,7 +6,7 @@ theta = [x/100 for x in range(1000, 9000)]
 
 # Hematita
 
-filepath_hematita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_17_05_2022/Base_Dados/Hematita'
+filepath_hematita = 'Base_Dados/Hematita/'
 ds_hem = os.listdir(filepath_hematita)
 tam_hem = len(ds_hem)
 
@@ -14,7 +14,7 @@ data_hem = np.zeros((tam_hem, len(theta)))
 hem_feature = []
 
 for i in range(0, tam_hem):
-    df = pd.read_csv('Base_Dados/Hematita/' + ds_hem[i], sep=';')
+    df = pd.read_csv(filepath_hematita + ds_hem[i], sep=';')
 
     for j in range(len(df['2-THETA'])):
         for k in range(len(theta)): 
@@ -26,7 +26,7 @@ for i in range(0, tam_hem):
 
 # Magnetita
 
-filepath_magnetita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_17_05_2022/Base_Dados/Magnetita'
+filepath_magnetita = 'Base_Dados/Magnetita/'
 ds_mag = os.listdir(filepath_magnetita)
 tam_mag = len(ds_mag)
 
@@ -34,7 +34,7 @@ data_mag = np.zeros((tam_mag, len(theta)))
 mag_feature = []
 
 for i in range(0, tam_mag):
-    df = pd.read_csv('Base_Dados/Magnetita/' + ds_mag[i], sep=';')
+    df = pd.read_csv(filepath_magnetita + ds_mag[i], sep=';')
 
     for j in range(len(df['2-THETA'])):
         for k in range(len(theta)): 
@@ -46,7 +46,7 @@ for i in range(0, tam_mag):
 
 # Goetita
 
-filepath_goetita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_17_05_2022/Base_Dados/Goetita'
+filepath_goetita = 'Base_Dados/Goetita/'
 ds_goe = os.listdir(filepath_goetita)
 tam_goe = len(ds_goe)
 
@@ -54,7 +54,7 @@ data_goe = np.zeros((tam_goe, len(theta)))
 goe_feature = []
 
 for i in range(0, tam_goe):
-    df = pd.read_csv('Base_Dados/Goetita/' + ds_goe[i], sep=';')
+    df = pd.read_csv(filepath_goetita + ds_goe[i], sep=';')
 
     for j in range(len(df['2-THETA'])):
         for k in range(len(theta)): 
@@ -66,7 +66,7 @@ for i in range(0, tam_goe):
 
 # Calcita
 
-filepath_calcita = 'C:/Users/Marcos Tassan/Documents/Mariana/rdx_17_05_2022/rdx_17_05_2022/Base_Dados/Calcita'
+filepath_calcita = 'Base_Dados/Calcita/'
 ds_cal = os.listdir(filepath_calcita)
 tam_cal = len(ds_cal)
 
@@ -74,7 +74,7 @@ data_cal = np.zeros((tam_cal, len(theta)))
 cal_feature = []
 
 for i in range(0, tam_cal):
-    df = pd.read_csv('Base_Dados/Calcita/' + ds_cal[i], sep=';')
+    df = pd.read_csv(filepath_calcita + ds_cal[i], sep=';')
 
     for j in range(len(df['2-THETA'])):
         for k in range(len(theta)): 
@@ -82,4 +82,4 @@ for i in range(0, tam_cal):
                 data_cal[i][k] = df['2-THETA'][j] 
     for m in range(10):
       cal_feature.append(df['INTENSITY'][m])
-    cal_feature.append(len(df['INTENSITY'])) 
+    cal_feature.append(len(df['INTENSITY']))
